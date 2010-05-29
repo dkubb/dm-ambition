278d6d1f36cd5885f3820755fe71d52a0627b6b2
Dan Kubb <dan.kubb@gmail.com>
2010-05-28 19:05:32 -0700

    Removed code to make lib backwards compatible with 0.9.x

--- lib/dm-ambition.rb
+++ lib/dm-ambition.rb
@@ -15,32 +15,4 @@ module DataMapper
   class Query
     include Ambition::Query
   end
-
-  if Gem::Version.new(DataMapper::VERSION) < Gem::Version.new('0.10')
-    module Model
-      def self.descendants
-        Resource.descendants
-      end
-    end
-
-    class Collection
-      def new_collection(query, resources = nil, &block)
-        collection = self.class.new(query, &block)
-
-        if resources
-          collection.replace(resources)
-        end
-
-        collection
-      end
-    end
-
-    module Resource
-      def saved?
-        !new_record?
-      end
-
-      alias new? new_record?
-    end
-  end
 end
